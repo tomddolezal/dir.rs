30f029a041369015461fd5ab36b6cb35f71be8ec
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2017-10-18 20:41:24 -0400

    Canonicalize given path; Output UNC absolute path

--- dir.rs
+++ dir.rs
@@ -1,12 +1,15 @@
 use std::env;
+use std::fs;
+use std::path::PathBuf;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut dir = env::current_dir();
 
     if args.len() > 1 {
-        println!("{}", args[1]);
-    } else {
-        println!("{}", env::current_dir().unwrap().display());
+        dir = fs::canonicalize(PathBuf::from(&args[1]));
     }
+
+    println!("{}", dir.unwrap().display());
 }
 
